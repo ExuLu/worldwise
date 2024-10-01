18d503a1a2a791ce027e71318cb361a723a7289b
Alena Sharai <exultantislupus@gmail.com>
2024-10-01 23:11:03 +0400

    feat: add country list with country items

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from './pages/AppLayout.jsx';
 import Login from './pages/Login.jsx';
 import CityList from './components/CityList.jsx';
 import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList.jsx';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -47,7 +48,7 @@ const App = () => {
               path='cities'
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
-            <Route path='countries' element={<p>List of countries</p>} />
+            <Route path='countries' element={<CountryList cities={cities} />} />
             <Route path='form' element={<p>Form</p>} />
           </Route>
           <Route path='*' element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,36 @@
+import styles from './CountryList.module.css';
+import PropTypes from 'prop-types';
+import Message from './Message';
+import Spinner from './Spinner';
+import CountryItem from './CountryItem';
+
+const CountryList = ({ cities, isLoading }) => {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message='Add your first city by clicking on a city on the map' />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+
+    return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.country} country={country} />
+      ))}
+    </ul>
+  );
+};
+
+CountryList.propTypes = {
+  cities: PropTypes.arrayOf(PropTypes.object),
+  isLoading: PropTypes.bool,
+};
+
+export default CountryList;
