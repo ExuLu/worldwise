df9cc9a30d78803729787c60c130f0b6089e3562
Alena Sharai <exultantislupus@gmail.com>
2024-10-02 20:37:34 +0400

    feat: add useNavigation to navigate from map to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './pages/Product.jsx';
-import Pricing from './pages/Pricing.jsx';
-import Homepage from './pages/Homepage.jsx';
-import PageNotFound from './pages/PageNotFound.jsx';
-import AppLayout from './pages/AppLayout.jsx';
-import Login from './pages/Login.jsx';
-import CityList from './components/CityList.jsx';
 import { useEffect, useState } from 'react';
-import CountryList from './components/CountryList.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import City from './components/City.jsx';
+import CityList from './components/CityList.jsx';
+import CountryList from './components/CountryList.jsx';
+import Form from './components/Form.jsx';
+
+import AppLayout from './pages/AppLayout.jsx';
+import Homepage from './pages/Homepage.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Product from './pages/Product.jsx';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -51,7 +54,7 @@ const App = () => {
             />
             <Route path='cities/:id' element={<City />} />
             <Route path='countries' element={<CountryList cities={cities} />} />
-            <Route path='form' element={<p>Form</p>} />
+            <Route path='form' element={<Form />} />
           </Route>
           <Route path='*' element={<PageNotFound />} />
         </Routes>
