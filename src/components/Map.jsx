67476823b4fe70e71d854fd0fbd8a9b66786fb27
Alena Sharai <exultantislupus@gmail.com>
2024-10-08 11:55:35 +0400

    feat: render button conditionally

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -33,9 +33,11 @@ const Map = () => {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type='position' onClick={getPosition}>
-        {isLoadingPosition ? 'Loading...' : 'Use your position'}
-      </Button>
+      {!geolocationPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your position'}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
