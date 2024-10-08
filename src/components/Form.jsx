e40d6f6175adff97c0058d1b937cb56124a23d8a
Alena Sharai <exultantislupus@gmail.com>
2024-10-08 19:59:14 +0400

    refactor: adjust input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -84,12 +84,12 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor='date'>When did you go to {cityName}?</label>
-        {/* <input
+        <DatePicker
           id='date'
-          onChange={(e) => setDate(e.target.value)}
-          value={formatDate(date)}
-        /> */}
-        <DatePicker onChange={(date) => setDate(date)} selected={date} />
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat='dd/MM/yyyy'
+        />
       </div>
 
       <div className={styles.row}>
