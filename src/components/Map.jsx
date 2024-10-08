dc69ae8f0a61fbe5d2585c5c624a36873f755b51
Alena Sharai <exultantislupus@gmail.com>
2024-10-08 16:17:20 +0400

    refactor: create useUrlPosition hook to get position from url

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,19 +8,17 @@ import ChangeCenter from './ChangeCenter';
 import DetectClick from './DetectClick';
 import { useGeolocation } from '../hooks/useGeolocation';
 import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 const Map = () => {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const { lat: mapLat, lng: mapLng } = useUrlPosition();
 
   useEffect(() => {
     if (mapLng && mapLat) setMapPosition([mapLat, mapLng]);
