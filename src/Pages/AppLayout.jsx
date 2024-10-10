c451074de83ba339e393f434249084fef4a23b5e
Alena Sharai <exultantislupus@gmail.com>
2024-10-10 16:55:23 +0400

    feat: add logout functionality to User component and add it to map

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -2,10 +2,12 @@ import Sidebar from '../components/Sidebar';
 import Map from '../components/Map';
 
 import styles from './AppLayout.module.css';
+import User from '../components/User';
 
 const AppLayout = () => (
   <div className={styles.app}>
     <Sidebar />
+    <User />
     <Map />
   </div>
 );
