06717f5a2b63f99fc72f5a9a39b4de9b4fb4090c
Alena Sharai <exultantislupus@gmail.com>
2024-10-10 16:18:58 +0400

    feat: add reducer to auth context and manage states

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,10 +1,39 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 import PropTypes from 'prop-types';
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'login':
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case 'logout':
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error('Unknown action');
+  }
+};
+
 const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const login = (email, password) => {};
+
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
